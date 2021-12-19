807e65ddb0a9ea2629a8f222cee6e22c7c8b963b
Martin A <martin.apalkov@gmail.com>
2021-12-19 19:57:52 +0200

    get data

--- simple-app-angular/src/app/app.component.ts
+++ simple-app-angular/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  constructor(private http: HttpClient) { }
   title = 'simple-app-angular';
+  usersDataUrl = 'http://localhost:8080/demo/all';
+  data = {};
+
+  ngOnInit() {
+    this.getUsers().subscribe(
+      (data) => {
+        console.log(data);
+        this.data = data;
+      }
+    );
+  }
+
+  getUsers() {
+    return this.http.get(this.usersDataUrl);
+  }
 }
